308df817a41f306cc663788283345ca121413172
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-27 10:42:59 -0300

    Fazendo upload de imagens com form data

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,10 @@
 const axios = require("axios");
 const slugify = require("slugify");
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
   const {JSDOM} = jsdom;
@@ -98,10 +102,42 @@ async function createGames(products) {
           ...(await getGameInfo(product.slug)),
         });
 
+        await setImage({image: product.image, game});
+        await Promise.all(
+          product.gallery.slice(0, 5).map(url => setImage({image: url, game, field: "gallery"}))
+        );
+
+        await timeout(2000);
+
         return game;
       }
     })
   );
+}
+
+async function setImage({ image, game, field = "cover" }) {
+  const url = `https:${image}_bg_crop_1680x655.jpg`;
+  const { data } = await axios.get(url, { responseType: "arraybuffer" });
+  const buffer = Buffer.from(data, "base64");
+
+  const FormData = require("form-data");
+  const formData = new FormData();
+
+  formData.append("refId", game.id);
+  formData.append("ref", "game");
+  formData.append("field", field);
+  formData.append("files", buffer, { filename: `${game.slug}.jpg` });
+
+  console.info(`Uploading ${field} image: ${game.slug}.jpg`);
+
+  await axios({
+    method: "POST",
+    url: `http://${strapi.config.host}:${strapi.config.port}/upload`,
+    data: formData,
+    headers: {
+      "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+    },
+  });
 }
 
 module.exports = {
@@ -115,8 +151,8 @@ module.exports = {
     // await create(products[10].publisher, "publisher");
     // await create(products[10].developer, "developer");
 
-    await createManyToManyData([products[7]], [products[8]])
-    await createGames([products[7]], [products[8]])
+    await createManyToManyData([products[4]], [products[5]])
+    await createGames([products[4]], [products[5]])
 
    // console.log(await getGameInfo(products[1].slug));
   }
